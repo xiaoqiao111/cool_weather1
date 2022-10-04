358e6ad8b309de401c3693edcfc1590c83acdd45
xiaoqiao <1499798703@qq.com>
2022-10-04 15:25:07 +0800

    second commit

---
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -0,0 +1,35 @@
+package com.example.coolweather.util;
+
+import android.text.TextUtils;
+
+import com.example.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+    /*解析和处理服务器返回的省级数据*/
+    public static boolean handleProvinceResponse(String response)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i<allProvinces.length();i++)
+                {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    Province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
